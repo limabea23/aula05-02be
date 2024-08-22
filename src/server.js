d11e0fb4250426a620629e7e5b5c2d9f7523d720
Beatriz Lima <beatrizlima.tds1.senai@gmail.com>
2024-08-22 11:40:08 -0300

    class is finished

--- src/server.js
+++ src/server.js
@@ -13,6 +13,10 @@ app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!"})
 })
 
+app.post("/2tds1", (req, res) => {
+    return res.status(333).send({ message: "Hello, 2TDS1 - só os dev!"})
+})
+
 app.listen(port, () => {
     console.log(`🤠 Server started on http://localhost:{port}`)
 })
