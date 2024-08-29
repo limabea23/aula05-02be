585cca2eba083658669f529335f45e0b42104a74
Beatriz Lima <beatrizlima.tds1.senai@gmail.com>
2024-08-29 15:51:39 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -9,8 +9,55 @@ const app = express()
 
 app.use(express.json())
 
+const guloseimas = [
+    {
+        id: 1,
+        nome: "Trufa",
+        preco: 8.50, 
+    },
+    {
+        id: 2,
+        nome: "Jujuba",
+        preco: 24.90,
+    },
+    {
+        id: 3,
+        nome:"Marshmellow",
+        preco: 18.33,
+    }
+]
+
+const filmesMarcantes = [
+    {
+        id: 1023,
+        nome: "A Cinco Passos de Você",
+        genero: "Drama",
+        emCartaz: false
+    },
+    {
+        id: 1024,
+        nome: "Viva! A vida é uma festa",
+        genero: "Infantil/Fantasia",
+        emCartaz: false
+    },
+    {
+        id: 1025,
+        nome: "As Metas de Jessica Darling",
+        genero: "Infantil/Comédia",
+        emCartaz: false
+    },
+]
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!"})
+})
+
+app.get("/doces", (req, res) => {
+    return res.status(200).send(guloseimas)
+})
+
+app.get("/filmes", (req, res) => {
+    return res.status(200).send(filmesMarcantes)
 })
 
 app.post("/2tds1", (req, res) => {
@@ -18,5 +65,5 @@ app.post("/2tds1", (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`🤠 Server started on http://localhost:{port}`)
+    console.log(`🤠 Server started on http://localhost:${port}`)
 })
