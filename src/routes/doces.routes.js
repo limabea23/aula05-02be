ab214f83fa90f4044dd57ec07561af766f792b8f
Beatriz Lima <beatrizlima.tds1.senai@gmail.com>
2024-08-29 17:09:20 -0300

    refactor: alter doces routes structure

---
+++ src/routes/doces.routes.js
@@ -0,0 +1,34 @@
+const guloseimas = [
+    {
+        id: 1,
+        nome: "Trufa",
+        preco: 8.50, 
+    },
+    {
+        id: 2,
+        nome: "Jujuba",
+        preco: 24.90,
+    },
+    {
+        id: 3,
+        nome:"Marshmellow",
+        preco: 18.33,
+    }
+]
+
+app.get("/doces", (req, res) => {
+    return res.status(200).send(guloseimas)
+})
+app.post("/doces", (req, res) => {
+    const {nome, preco} = req.body
+
+    const novoDoce = {
+        id: guloseimas.length + 1, 
+        nome: nome, 
+        preco: preco,
+    }
+
+    guloseimas.push(novoDoce)
+    
+    return res.status(201).send(guloseimas)
+})
--- src/server.js
+++ src/server.js
@@ -9,23 +9,7 @@ const app = express()
 
 app.use(express.json())
 
-const guloseimas = [
-    {
-        id: 1,
-        nome: "Trufa",
-        preco: 8.50, 
-    },
-    {
-        id: 2,
-        nome: "Jujuba",
-        preco: 24.90,
-    },
-    {
-        id: 3,
-        nome:"Marshmellow",
-        preco: 18.33,
-    }
-]
+
 
 const filmesMarcantes = [
     {
@@ -52,22 +36,7 @@ app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!"})
 })
 
-app.get("/doces", (req, res) => {
-    return res.status(200).send(guloseimas)
-})
-app.post("/doces", (req, res) => {
-    const {nome, preco} = req.body
 
-    const novoDoce = {
-        id: guloseimas.length + 1, 
-        nome: nome, 
-        preco: preco,
-    }
-
-    guloseimas.push(novoDoce)
-    
-    return res.status(201).send(guloseimas)
-})
 
 app.get("/filmes", (req, res) => {
     return res.status(200).send(filmesMarcantes)
