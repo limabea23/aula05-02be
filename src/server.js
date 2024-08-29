5134ce32284570276209b0ef16a41d28662206b0
Beatriz Lima <beatrizlima.tds1.senai@gmail.com>
2024-08-29 17:11:33 -0300

    refactor: alter filmesMarcantes routes structure

--- src/server.js
+++ src/server.js
@@ -11,26 +11,7 @@ app.use(express.json())
 
 
 
-const filmesMarcantes = [
-    {
-        id: 1023,
-        nome: "A Cinco Passos de Você",
-        genero: "Drama",
-        emCartaz: false
-    },
-    {
-        id: 1024,
-        nome: "Viva! A vida é uma festa",
-        genero: "Infantil/Fantasia",
-        emCartaz: false
-    },
-    {
-        id: 1025,
-        nome: "As Metas de Jessica Darling",
-        genero: "Infantil/Comédia",
-        emCartaz: false
-    },
-]
+
 
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!"})
@@ -38,9 +19,7 @@ app.get("/", (req, res) => {
 
 
 
-app.get("/filmes", (req, res) => {
-    return res.status(200).send(filmesMarcantes)
-})
+
 
 app.post("/2tds1", (req, res) => {
     return res.status(333).send({ message: "Hello, 2TDS1 - só os dev!"})
