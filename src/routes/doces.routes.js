cfb3999f4564ffe496b025e734e01cdca960d2c6
Beatriz Lima <beatrizlima.tds1.senai@gmail.com>
2024-09-05 09:06:10 -0300

    feat: create routes config

--- src/routes/doces.routes.js
+++ src/routes/doces.routes.js
@@ -1,3 +1,7 @@
+import { Router } from "express"
+
+const docesRoutes = Router()
+
 const guloseimas = [
     {
         id: 1,
@@ -16,10 +20,10 @@ const guloseimas = [
     }
 ]
 
-app.get("/doces", (req, res) => {
+docesRoutes.get("/", (req, res) => {
     return res.status(200).send(guloseimas)
 })
-app.post("/doces", (req, res) => {
+docesRoutes.post("/", (req, res) => {
     const {nome, preco} = req.body
 
     const novoDoce = {
@@ -32,3 +36,5 @@ app.post("/doces", (req, res) => {
     
     return res.status(201).send(guloseimas)
 })
+
+export default docesRoutes
--- src/server.js
+++ src/server.js
@@ -1,24 +1,15 @@
 import express from "express"
 import { config } from "dotenv"
 
+import routes from "./routes/index.routes.js"
+
 config()
 
 const port = process.env.PORT || 3000
 
 const app = express()
-
+app.use(routes)
 app.use(express.json())
-
-
-
-
-
-app.get("/", (req, res) => {
-    return res.status(200).send({ message: "Hello, World!"})
-})
-
-
-
 
 
 app.post("/2tds1", (req, res) => {
